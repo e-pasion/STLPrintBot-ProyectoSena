80401cafe56e2b776ed78c02d3fbdb1ffcddc2c0
Elias Pasion <elian.pbh@gmail.com>
2023-08-18 17:06:09 -0500

    add firebaseconfig

---
+++ servidor/src/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCZfDMwNcCf7gzmEv8g9UTSJX06WoXIYHY",
+    authDomain: "stlprintbot-b37de.firebaseapp.com",
+    projectId: "stlprintbot-b37de",
+    storageBucket: "stlprintbot-b37de.appspot.com",
+    messagingSenderId: "809695087506",
+    appId: "1:809695087506:web:84740df7a5270d63884f4b"
+  };
+  
